c13cd2ae31d49657cf4ca5487f4a988dfd66ef73
Joshua Ondieki <joshuaondieki8@gmail.com>
2017-01-25 00:16:02 +0300

    Add colors and list table

--- Quizdella.py
+++ Quizdella.py
@@ -1,29 +1,67 @@
 import os
 import json
 import cmd
-current_path = os.path.dirname(os.path.abspath(__file__))
+import time
+from termcolor import *
+from pyfiglet import *
+from prettytable import *
+from firebase import *
+from colorama import *
 
+
+cprint(figlet_format('Quiz', font='poison'),'red', "on_green")
+cprint(figlet_format("Della", font="poison"), "red", "on_green")
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+della_path = current_path + "/dellas/"
+users={}
 class QuizApp(cmd.Cmd):
     def quiz_list(self):
-        quiz_list_path=current_path+"/dellas/"
+        print(Back.GREEN)
+        print(Fore.BLUE)
+        qlist=PrettyTable(["Quiz","Origin"])
+        quiz_list_path = current_path+"/dellas/"
         for file in os.listdir(quiz_list_path):
-            print (file)
-    def take_quiz(self,quiz):
+            qlist.add_row([file,"Quizdella"])
+        print(qlist)
+        print(Style.RESET_ALL)
+
+    def take_quiz(self , quiz):
+
         quiz_path=current_path+"/dellas/"+quiz+".json"
         with open(quiz_path) as quiz:
             della = json.load(quiz)
-        questions=della.keys()
+        questions=list(della.keys())
         user_score=0
         total_questions=int(len(questions))
+        print(total_questions)
         question_number=0
-        while question_number<total_questions:
-            answer=raw_input("Type answer here: ")
-            print (della[questions[question_number]])
-            if answer==della[questions[question_number]]:
+        time_out=10*total_questions
+        timer_start=time.time()
+        while question_number < total_questions:
+            print(Back.YELLOW)
+            print(Fore.BLUE)
+            print(questions[question_number])
+            answer = input("Type your answer: ")
+            print(Style.RESET_ALL)
+            if answer == della[questions[question_number]]:
+                print(Fore.GREEN)
                 print("Correct!")
-                user_score +=1
+                print(Style.RESET_ALL)
+                user_score += 1
             else:
+                print(Fore.RED)
                 print("Wrong answer")
-            question_number+=1
-        print ("Your score is %s")%(str(user_score))
+                print(Style.RESET_ALL)
+            question_number += 1
+            timer=time.time()
+            if time_out<(timer-timer_start):
+                print(Fore.RED)
+                print("Sorry timed out!")
+                print(Style.RESET_ALL)
+                time.sleep(2)
+                break
+        user_score=str(user_score)
+        print ("Your score is "+user_score)
 app=QuizApp()
+app.quiz_list()
